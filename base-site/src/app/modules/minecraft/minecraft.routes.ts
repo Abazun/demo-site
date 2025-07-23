af0ae663ad204256f6da7e3054e2fdb6afeb70d3
Devin <neomalco@gmail.com>
2025-07-23 00:35:37 -0400

    fixing branch. Removing extra files part 2.

--- base-site/src/app/modules/minecraft/minecraft.routes.ts
+++
@@ -1,6 +0,0 @@
-import { MinecraftPageComponent } from "./components/minecraft-page/minecraft-page.component";
-
-export const minecraftRoutes = [
-  { path: 'mcft', redirectTo: 'minecraft', pathMatch: 'full'},
-  { path: 'minecraft', component: MinecraftPageComponent }
-]
